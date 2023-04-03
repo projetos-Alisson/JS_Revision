6a9ea0e6fb0acadbc233db5f6e2fe030f5c94239
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2023-04-03 20:08:17 -0300

    Add files via upload

---
+++ JS exe/Laço de Repetição/Tabuada1à10.js
@@ -0,0 +1,12 @@
+// function printTable(n){
+//     for(var i= 1; i<=10; i++){
+//     var row = n + "*" +i+ "=" +n*i;
+//     console.log(row)
+//     }
+// }
+
+// for (var i = 1; i <= 10; i++){
+//     printTable(i);
+//     console.log("")
+// }
+
---
+++ JS exe/porcentagem.js
@@ -0,0 +1,12 @@
+//GIVE A ARGUMENT TO GET THE RESULT
+
+// function porcentagem(num,per){
+//     return (num/100)*per;
+// }
+
+// console.log(porcentagem(300,40))
+
+
+
+// ARGUMENT - num: 300;
+//            per: 40;
